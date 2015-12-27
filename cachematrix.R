9cafec6763e56d1b0764ca073a11a972031d1b19
Ben Abbott <ben@benabbottanalytics.com>
2015-12-26 22:00:08 -0500

    reformatted comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,14 @@
 ## makeCacheMatrix is a set of functions that stores and returns values
 ## cacheSolve returns the inverse matrix
 
-## makeCacheMatrix stores a list of 4 functions
-## get returns the matrix x
-## set stores the value of the matrix x
-## get inverse returns the value of the inverse matrix m
-## set inverse stores the value of the inverse matrix m
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## makeCacheMatrix stores a list of 4 functions
+        ## get returns the matrix x
+        ## set stores the value of the matrix x
+        ## get inverse returns the value of the inverse matrix m
+        ## set inverse stores the value of the inverse matrix m
+        
         inv <- NULL
         set <- function(y) {
                 x <<- y
@@ -21,12 +21,12 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-
-## cacheSolve returns the existing inverse matrix if one exists
-## cacheSolve calculates the inverse matrix and returns it if one
-## didn't exist previously
-
 cacheSolve <- function(x, ...) {
+        
+        ## cacheSolve returns the existing inverse matrix if one exists
+        ## cacheSolve calculates the inverse matrix and returns it if one
+        ## didn't exist previously
+        
         inv <- x$getinverse()
         if(!is.null(inv)) {
                 return(inv)
